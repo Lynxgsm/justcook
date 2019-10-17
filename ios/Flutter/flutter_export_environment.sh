39152e7a9223a2d95861dfb1127db76a3104fb1d
etech <etech@etechs-MacBook-Pro-2.local>
2019-10-17 13:47:50 +0300

    blank project

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/Shared/lib/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/etech/Documents/codemagic/justcook/justcook"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/Shared/lib/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
